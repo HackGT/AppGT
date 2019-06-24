5eac0e120c040c91666e5ba1969938a1feafb4fd
Nicholas714 <grananicholas@gmail.com>
2019-06-23 20:30:12 -0400

    fix iOS 13 crashing

--- screens/Event.js
+++ screens/Event.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
-export default class Event extends Component<Props> {
+export default class Event extends Component<Props> {    
     render() {
         const event = this.props.navigation.getParam('event', '');
 
