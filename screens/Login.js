863f51bf118142ca9c7ab3ec094488baed6d4d6a
Nicholas714 <grananicholas@gmail.com>
2019-07-16 19:56:59 -0400

    add react-native-app-auth and simple login flow

--- screens/Login.js
+++ screens/Login.js
@@ -1,21 +1,55 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, Button } from 'react-native';
+import { authorize } from 'react-native-app-auth';
 
-export default class Login extends Component<Props> {    
+
+/*
+        const config = {
+            issuer: '<YOUR_ISSUER_URL>',
+            clientId: '<YOUR_CLIENT_ID>',
+            redirectUrl: '<YOUR_REDIRECT_URL>',
+            scopes: ['<YOUR_SCOPES_ARRAY>'],
+        };
+
+*/
+
+const config = {
+    issuer: 'https://demo.identityserver.io',
+    clientId: 'native.code',
+    redirectUrl: 'io.identityserver.demo:/oauthredirect',
+    additionalParameters: {},
+    scopes: ['openid', 'profile', 'email', 'offline_access']
+  
+    // serviceConfiguration: {
+    //   authorizationEndpoint: 'https://demo.identityserver.io/connect/authorize',
+    //   tokenEndpoint: 'https://demo.identityserver.io/connect/token',
+    //   revocationEndpoint: 'https://demo.identityserver.io/connect/revoke'
+    // }
+  };
+
+  
+export default class Login extends Component<Props> {
 
     static navigationOptions = {
         title: 'Login',
         headerLeft: null
-      };
+    };
+
+    onClickLogin = async () => {
+        console.log("test")
+        // use the client to make the auth request and receive the authState
+        try {
+            const result = await authorize(config);
+            // result includes accessToken, accessTokenExpirationDate and refreshToken
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     render() {
-        
+
         return (
-            <View>
-                <Text>
-                    Login!
-                </Text>
-            </View>
+            <Button onPress={this.onClickLogin} title="Login" />
         )
     }
 }
