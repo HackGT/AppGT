a6ad3245310418276c36504e4aaed77cfe322a04
Stephanie <almeidasmail@gmail.com>
2019-07-23 13:46:57 -0400

    Add onesignal

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Text} from "react-native";
+import OneSignal from 'react-native-onesignal'; // Import package from node modules
 
 import Workshops from "./screens/Workshops";
 import Home from './screens/Home';
@@ -45,7 +46,7 @@ const TabNavigator = createBottomTabNavigator({
             icon = faList
             break;
           case "Notifications":
-            icon = faBell 
+            icon = faBell
             break;
           default:
             icon = null;
@@ -60,6 +61,37 @@ const TabNavigator = createBottomTabNavigator({
 const AppContainer = createAppContainer(TabNavigator);
 
 export default class App extends Component<Props> {
+
+  constructor(properties) {
+      super(properties);
+      //TODO: replace with env
+      OneSignal.init("d4d45469-e177-4ab7-92f2-42f619fd39b0");
+
+      OneSignal.addEventListener('received', this.onReceived);
+      OneSignal.addEventListener('opened', this.onOpened);
+      OneSignal.addEventListener('ids', this.onIds);
+    }
+
+    componentWillUnmount() {
+      OneSignal.removeEventListener('received', this.onReceived);
+      OneSignal.removeEventListener('opened', this.onOpened);
+      OneSignal.removeEventListener('ids', this.onIds);
+    }
+
+    onReceived(notification) {
+      console.log("Notification received: ", notification);
+    }
+
+    onOpened(openResult) {
+      console.log('Message: ', openResult.notification.payload.body);
+      console.log('Data: ', openResult.notification.payload.additionalData);
+      console.log('isActive: ', openResult.notification.isAppInFocus);
+      console.log('openResult: ', openResult);
+    }
+
+    onIds(device) {
+      console.log('Device info: ', device);
+    }
 
   render() {
     return (
