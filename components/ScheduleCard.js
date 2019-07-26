562b78aa92b22d2bae1e6c985c85e6db1219cc05
Stephanie <almeidasmail@gmail.com>
2019-07-26 14:02:22 -0400

    Added functionality for sending/deleting tags

--- components/ScheduleCard.js
+++ components/ScheduleCard.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, TouchableHighlight } from 'react-native';
+import { Text, View, StyleSheet, TouchableHighlight, Switch } from 'react-native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faPlusCircle, faStar } from '@fortawesome/free-solid-svg-icons'
 import { thisExpression } from '@babel/types';
+import OneSignal from 'react-native-onesignal';
 
 export default class ScheduleCard extends Component<Props> {
 
+  state = {
+    switchValue: false,
+  }
+
   onClick = () => {
     this.props.onClick(this.props.item);
+  }
+
+  onSwitchToggle = () => {
+
   }
 
   render() {
@@ -21,6 +30,19 @@ export default class ScheduleCard extends Component<Props> {
             position: "absolute", alignSelf: "flex-end",
             flex: 1, padding: 8
           }}>
+            <Text>Subscribe to Event?</Text>
+            <Switch
+            value={this.state.switchValue}
+            onValueChange ={(switchValue)=> {
+              this.setState({switchValue});
+              if (switchValue) OneSignal.sendTag(this.props.title + "eventid", "subscribed");
+              else OneSignal.deleteTag(this.props.title + "eventid");
+              OneSignal.getTags((receivedTags) => {
+                  console.log(receivedTags);
+              });
+              console.log(switchValue);
+            }}/>
+
           </View>
 
           {title}
