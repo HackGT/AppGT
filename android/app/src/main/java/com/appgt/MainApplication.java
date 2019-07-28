3012ef8a578636a2cd87de690bb816aa8ad92c5c
Stephanie <almeidasmail@gmail.com>
2019-07-28 17:05:49 -0400

    install messaging for android

--- android/app/src/main/java/com/appgt/MainApplication.java
+++ android/app/src/main/java/com/appgt/MainApplication.java
@@ -14,6 +14,7 @@ import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 
 import java.util.List;
 
@@ -31,6 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
+      packages.add(new RNFirebaseMessagingPackage());
       return packages;
     }
 
