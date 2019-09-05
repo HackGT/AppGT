c4952a4f407ee1e6a90a3ec34044b9b6a87f46bd
Meha Agrawal <agra.meha@hotmail.com>
2019-09-05 00:42:09 -0400

    Add a themes folder with a default colors file

---
+++ themes/colors.js
@@ -0,0 +1,13 @@
+/**
+ * @providesModule Colors
+ */
+const colors = {
+  highlightedBlue: "#5DBCD2",
+  darkGrayText: "#4F4F4F",
+  lightGrayText: "#797979",
+  lightGrayBackgroundBehindTagText: "#E9E9E9",
+  yellow: "#D2B35D",
+  red: "#D25D5D",
+  lightGrayImages: "#D3D3D3"
+};
+export default colors;
