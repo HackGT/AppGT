b1cbbb849708fe3217b05bba9a72a7831c267638
Meha Agrawal <agra.meha@hotmail.com>
2019-09-05 09:07:52 -0400

    Rename colors

--- themes/colors.js
+++ themes/colors.js
@@ -2,11 +2,11 @@
  * @providesModule Colors
  */
 const colors = {
-  highlightedBlue: "#5DBCD2",
+  primaryBlue: "#5DBCD2",
   darkGrayText: "#4F4F4F",
   lightGrayText: "#797979",
   lightGrayBackgroundBehindTagText: "#E9E9E9",
-  yellow: "#D2B35D",
+  primaryStar: "#D2B35D",
   red: "#D25D5D",
   lightGrayImages: "#D3D3D3"
 };
