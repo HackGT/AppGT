0ac9cb5de628ac382cdec9757bca3285c1694c84
Nicholas714 <grananicholas@gmail.com>
2019-09-15 01:38:30 +0200

    fix index

--- screens/index.js
+++ screens/index.js
@@ -1,4 +1,5 @@
-export { default as Event } from './Event';
-export { default as Home } from './Home';
-export { default as Notifications } from './Notifications';
-export { default as Schedule } from './Schedule';
+export { default as Event } from "./Event";
+export { default as Home } from "./Home";
+export { default as Notifications } from "./Notifications";
+export { default as Schedule } from "./Schedule";
+export { default as Login } from "./Login";
