b17af98d358b13dc8654c6a997395c3b62cd271b
Meha Agrawal <agra.meha@hotmail.com>
2019-10-16 22:09:56 -0400

    Fix hyperlinks for restaurant names

--- components/events/Meal.js
+++ components/events/Meal.js
@@ -1,14 +1,17 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Linking } from "react-native";
 import { StyledText } from "../";
 import { styleguide } from "../../styles";
 
 export default (Meal = ({ restaurantName, restaurantLink, menuItems }) => {
-  console.log(menuItems);
   return (
     <View style={styles.content}>
-      <StyledText style={{ ...styleguide.text }}>{restaurantName}</StyledText>
-      <StyledText style={{ ...styleguide.text }}>{restaurantLink}</StyledText>
+      <View style={styles.restaurantNames}>
+        <StyledText style={styles.restaurantNameTitle}>
+          Restaurant Name:
+        </StyledText>
+        {makeHyperlinks(restaurantName, restaurantLink)}
+      </View>
       <View
         style={{
           flexDirection: "row",
@@ -25,6 +28,40 @@ export default (Meal = ({ restaurantName, restaurantLink, menuItems }) => {
   );
 });
 
+const makeHyperlinks = (restaurantName, restaurantLink) => {
+  console.log("in makeHyperlinks");
+  restaurantNameArr = restaurantName.split(", ");
+  restaurantLinkArr = restaurantLink.split(", ");
+  return restaurantNameArr.map((name, index) => {
+    if (index === restaurantNameArr.length - 1) {
+      if (index < restaurantLinkArr.length) {
+        return (
+          <StyledText
+            style={styles.link}
+            onPress={() => Linking.openURL(restaurantLinkArr[index])}
+          >
+            {name}
+          </StyledText>
+        );
+      } else {
+        return <StyledText style={styles.text}>{name}</StyledText>;
+      }
+    } else if (index < restaurantLinkArr.length) {
+      return (
+        <StyledText
+          style={styles.link}
+          onPress={() => Linking.openURL(restaurantLinkArr[index])}
+        >
+          {name},
+        </StyledText>
+      );
+    } else {
+      return <StyledText style={styles.text}>{name}, </StyledText>;
+    }
+  });
+  return restaurantName;
+};
+
 const styles = StyleSheet.create({
   content: {
     backgroundColor: "white",
@@ -33,5 +70,24 @@ const styles = StyleSheet.create({
     alignItems: "center",
     borderRadius: 4,
     borderColor: "rgba(0, 0, 0, 0.1)"
+  },
+  link: {
+    fontSize: 15,
+    marginBottom: 12,
+    marginLeft: 5,
+    color: "blue"
+  },
+  text: {
+    fontSize: 15,
+    marginLeft: 5,
+    marginBottom: 12
+  },
+  restaurantNames: {
+    flexDirection: "row",
+    flexWrap: "wrap"
+  },
+  restaurantNameTitle: {
+    fontSize: 15,
+    fontWeight: "bold"
   }
 });
