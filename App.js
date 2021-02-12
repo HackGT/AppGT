bca03bf0910a10b0e12bbc8b88c97dff6b6b3323
Nicholas Grana <grananicholas@gmail.com>
2021-02-11 21:39:53 -0500

    add 10s delay local notification

--- App.js
+++ App.js
@@ -21,6 +21,8 @@ import HackGTIcon from "./assets/HackGTIcon";
 import AsyncStorage from "@react-native-community/async-storage";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { authorize } from "react-native-app-auth";
+import PushNotificationIOS from "@react-native-community/push-notification-ios";
+import PushNotification from "react-native-push-notification";
 import {
   useDarkModeContext,
   useDynamicStyleSheet,
@@ -38,6 +40,56 @@ const config = {
     tokenEndpoint: `${authUrl}/oauth/token`,
   },
 };
+
+PushNotification.configure({
+  // (optional) Called when Token is generated (iOS and Android)
+  onRegister: function (token) {
+    console.log("TOKEN:", token);
+  },
+
+  // (required) Called when a remote is received or opened, or local notification is opened
+  onNotification: function (notification) {
+    console.log("NOTIFICATION:", notification);
+
+    // process the notification
+
+    // (required) Called when a remote is received or opened, or local notification is opened
+    notification.finish(PushNotificationIOS.FetchResult.NoData);
+  },
+
+  // (optional) Called when Registered Action is pressed and invokeApp is false, if true onNotification will be called (Android)
+  onAction: function (notification) {
+    console.log("ACTION:", notification.action);
+    console.log("NOTIFICATION:", notification);
+
+    // process the action
+  },
+
+  // (optional) Called when the user fails to register for remote notifications. Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
+  onRegistrationError: function (err) {
+    console.error(err.message, err);
+  },
+
+  // IOS ONLY (optional): default: all - Permissions to register.
+  permissions: {
+    alert: true,
+    badge: true,
+    sound: true,
+  },
+
+  // Should the initial notification be popped automatically
+  // default: true
+  popInitialNotification: true,
+
+  /**
+   * (optional) default: true
+   * - Specified if permissions (ios) and token (android and ios) will requested or not,
+   * - if not, you must call PushNotificationsHandler.requestPermissions() later
+   * - if you are not using remote notification or do not have Firebase installed, use this:
+   *     requestPermissions: Platform.OS === 'ios'
+   */
+  requestPermissions: true,
+});
 
 function HackGTitle() {
   return <HackGTIcon />;
@@ -71,7 +123,15 @@ function SchdeuleStackScreen({ navigation }) {
                   </TouchableOpacity>
                   <TouchableOpacity
                     style={{ paddingLeft: 10, paddingRight: 10 }}
-                    onPress={() => navigation.navigate("ScheduleSearch")}
+                    onPress={() => {
+                      // PushNotification.localNotificationSchedule({
+                      //   //... You can use all the options from localNotifications
+                      //   message: "My Notification Message", // (required)
+                      //   date: new Date(Date.now() + 10 * 1000), // in 60 secs
+                      //   allowWhileIdle: false, // (optional) set notification to work while on doze, default: false
+                      // });
+                      navigation.navigate("ScheduleSearch");
+                    }}
                   >
                     <SearchIcon
                       fill={dStyles.secondaryBackgroundColor.backgroundColor}
