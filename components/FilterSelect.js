beec6551c79bd7e1776e71b6086ad37328d0bc49
Nicholas Grana <grananicholas@gmail.com>
2021-03-08 15:10:15 -0500

    fix filterselect not working android

--- components/FilterSelect.js
+++ components/FilterSelect.js
@@ -1,6 +1,7 @@
-import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import React, { Component } from "react";
 import { ThemeContext, HackathonContext } from "../context";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default class FilterSelect extends Component {
   constructor() {
