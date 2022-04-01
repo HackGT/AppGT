b6ce026240dcc52e2613cd641897d67d90d82634
Rahil Patel <rahilpatel99@gmail.com>
2022-04-01 13:36:36 -0400

    made EventTypeView a functional component

--- schedule/EventTypeView.js
+++ schedule/EventTypeView.js
@@ -1,31 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 import { View, StyleSheet, Text } from "react-native";
 import Svg, { Circle } from "react-native-svg";
 
-export class EventTypeView extends Component {
-  render() {
-    const radius = 6;
-    const size = radius * 2;
-    const eventType = this.props.eventType ?? { name: "none", color: "gray" };
-    const color = eventType.color ?? "white";
+export function EventTypeView(props) {
+  const radius = 6;
+  const size = radius * 2;
+  const eventType = props.eventType ?? { name: "none", color: "gray" };
+  const color = eventType.color ?? "white";
 
-    return (
-      <View flexDirection="row">
-        <Svg height={size} width={size} style={styles.footerTopic}>
-          <Circle cx={radius} cy={radius} r={radius} fill={color} />
-        </Svg>
-        <Text
-          style={{
-            marginLeft: 7,
-            color: color,
-            fontFamily: "SpaceMono-Regular",
-          }}
-        >
-          {eventType.name}
-        </Text>
-      </View>
-    );
-  }
+  return (
+    <View flexDirection="row">
+      <Svg height={size} width={size} style={styles.footerTopic}>
+        <Circle cx={radius} cy={radius} r={radius} fill={color} />
+      </Svg>
+      <Text
+        style={{
+          marginLeft: 7,
+          color: color,
+          fontFamily: "SpaceMono-Regular",
+        }}
+      >
+        {eventType.name}
+      </Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
