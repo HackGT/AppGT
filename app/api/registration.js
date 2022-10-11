f973bacdcfad2cfe273e81434167215060f777f9
Ayush Goyal <agoyal2001@gmail.com>
2022-10-11 03:38:21 -0400

    Fix registration url not working for checkin

--- app/api/registration.js
+++ app/api/registration.js
@@ -1,12 +1,15 @@
-const REGISTRATION_URL = 'registration.api.hexlabs.org'
+const REGISTRATION_URL = "https://registration.api.hexlabs.org";
 
 export async function getApplication(token, hexathonId, userId) {
-    const response = await fetch(`${REGISTRATION_URL}/applications?hexathon=${hexathonId}&userId=${userId}`, {
-        method: "GET",
-        headers: {
-            Authorization: "Bearer " + token,
-        },
-    });
-    const json = await response.json();
-    return { json, status: response.status };
+  const response = await fetch(
+    `${REGISTRATION_URL}/applications?hexathon=${hexathonId}&userId=${userId}`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    }
+  );
+  const json = await response.json();
+  return { data: json, status: response.status };
 }
