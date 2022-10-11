5f2f018e41c6cb2e74c038f95c3635777536ccdb
Ayush Goyal <agoyal2001@gmail.com>
2022-10-11 03:39:06 -0400

    Add error handling and fix bugs for checkin qr/nfc

--- app/features/checkin/CheckInNFC.js
+++ app/features/checkin/CheckInNFC.js
@@ -1,114 +1,146 @@
-import React, { useContext } from 'react'
-import { View, Text, Image, TouchableOpacity } from 'react-native'
+import React, { useContext, useEffect } from "react";
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  Alert,
+  Dimensions,
+} from "react-native";
 import { ThemeContext } from "../../contexts/ThemeContext";
-import { initNfc, writeNFC } from '../../NFCScanning/nfc';
-import BadgeJudge from '../../../assets/images/BadgeJudge.png';
-import BadgeMentor from '../../../assets/images/BadgeMentor.png';
-import BadgeOrganizer from '../../../assets/images/BadgeOrganizer.png';
-import BadgeParticipant from '../../../assets/images/BadgeParticipant.png';
-import BadgeSponsor from '../../../assets/images/BadgeSponsor.png';
-import BadgeVolunteer from '../../../assets/images/BadgeVolunteer.png';
-
+import { initNfc, writeNFC } from "../../NFCScanning/nfc";
+import BadgeJudge from "../../../assets/images/BadgeJudge.png";
+import BadgeMentor from "../../../assets/images/BadgeMentor.png";
+import BadgeOrganizer from "../../../assets/images/BadgeOrganizer.png";
+import BadgeParticipant from "../../../assets/images/BadgeParticipant.png";
+import BadgeSponsor from "../../../assets/images/BadgeSponsor.png";
+import BadgeVolunteer from "../../../assets/images/BadgeVolunteer.png";
 
 export function CheckInNFC(props) {
-  const { application } = props.route.params
+  const { application } = props.route.params;
   const { dynamicStyles } = useContext(ThemeContext);
 
   useEffect(async () => {
+    const cleanUp = await initNfc((scannedText) => {
+      onScan(scannedText);
+    });
 
-    const cleanUp = await initNfc((scannedText) => { onScan(scannedText) })
+    return () => cleanUp();
+  }, []);
 
-    return () => cleanUp()
-  }, [])
-
-  const tagImage = (branch) => {
-    const source = null
-    switch(branch) {
-      case 'PARTICIPANT':
+  const TagImage = (props) => {
+    let source = null;
+    switch (props.branch) {
+      case "PARTICIPANT":
         source = BadgeParticipant;
         break;
-      case 'MENTOR':
+      case "MENTOR":
         source = BadgeMentor;
         break;
-      case 'JUDGE':
+      case "JUDGE":
         source = BadgeJudge;
         break;
-      case 'ORGANIZER':
+      case "ORGANIZER":
         source = BadgeOrganizer;
         break;
-      case 'SPONSOR':
+      case "SPONSOR":
         source = BadgeSponsor;
         break;
-      case 'VOLUNTEER':
+      case "VOLUNTEER":
         source = BadgeVolunteer;
         break;
       default:
         return null;
     }
     if (source === null) return null;
-    return <Image source={source} style={{ width: Dimensions.get("window").width - 80, resizeMode: 'contain', backgroundColor: 'red' }} />
-
-  }
+    return (
+      <Image
+        source={source}
+        style={{
+          alignSelf: "center",
+          width: Dimensions.get("window").width - 80,
+          resizeMode: "contain",
+        }}
+      />
+    );
+  };
 
   const createAlert = (message) =>
     Alert.alert("Error", message, [
       {
         text: "OK",
-        onPress: () => {
-          onPressScan()
-        },
       },
     ]);
 
   const onPressScan = async () => {
-    const success = await writeNFC(JSON.stringify({uid: application.userId}))
+    const success = await writeNFC(JSON.stringify({ uid: application.userId }));
     if (!success) {
-      createAlert("There was an issue writing to the badge. Try again.")
+      createAlert("There was an issue writing to the badge. Try again.");
     }
-  }
+  };
 
   return (
-    <View style={[dynamicStyles.backgroundColor, { flex: 1 }]}>
-      <View style={{ alignItems: 'center', paddingTop: 40 }}>
-        <Text style={{
+    <View
+      style={[
+        dynamicStyles.backgroundColor,
+        {
+          flex: 1,
+          paddingTop: 40,
+          paddingHorizontal: 20,
+        },
+      ]}
+    >
+      <Text
+        style={{
+          color: dynamicStyles.toggleText.color,
+          fontFamily: "SpaceMono-Bold",
+          fontSize: 16,
+        }}
+      >
+        {"Name: " + application.name}
+      </Text>
+      <Text
+        style={{
+          color: dynamicStyles.toggleText.color,
+          fontFamily: "SpaceMono-Bold",
+          fontSize: 16,
+        }}
+      >
+        {"Email: " + application.email}
+      </Text>
+      <Text
+        style={{
+          color: dynamicStyles.toggleText.color,
+          fontFamily: "SpaceMono-Bold",
+          fontSize: 16,
+        }}
+      >
+        {"User ID: " + application.userId}
+      </Text>
+      <Text
+        style={{
+          color: dynamicStyles.toggleText.color,
+          fontFamily: "SpaceMono-Bold",
+          fontSize: 16,
+        }}
+      >
+        {"Application Group: " +
+          application.confirmationBranch.applicationGroup}
+      </Text>
+      <TouchableOpacity onPress={onPressScan}>
+        <Text
+          style={{
             color: dynamicStyles.toggleText.color,
             fontFamily: "SpaceMono-Bold",
-            fontSize: 20
-        }}>
-          {'Name: ' + application.name}
+            fontSize: 20,
+            marginTop: 30,
+            alignSelf: "center",
+          }}
+        >
+          {"Scan Badge"}
         </Text>
-        <Text style={{
-          color: dynamicStyles.toggleText.color,
-          fontFamily: "SpaceMono-Bold",
-          fontSize: 20
-        }}>
-          {'Email: ' + application.email}
-        </Text>
-        <Text style={{
-          color: dynamicStyles.toggleText.color,
-          fontFamily: "SpaceMono-Bold",
-          fontSize: 20
-        }}>
-          {'User ID: ' + application.userId}
-        </Text>
-        <Text style={{
-          color: dynamicStyles.toggleText.color,
-          fontFamily: "SpaceMono-Bold",
-          fontSize: 20
-        }}>
-          {'Application Group: ' + application.confirmationBranch.applicationGroup}
-        </Text>
-        <TouchableOpacity onPress={onPressScan}>
-          <Text style={{
-            color: dynamicStyles.toggleText.color,
-            fontFamily: "SpaceMono-Bold",
-            fontSize: 20
-          }}>
-            {'Scan Badge'}
-          </Text>
-        </TouchableOpacity>
-        {tagImage(application.confirmationBranch.applicationGroups)}
-      </View>
+      </TouchableOpacity>
+      <TagImage branch={application.confirmationBranch.applicationGroup} />
     </View>
-  )
+  );
 }
--- app/features/checkin/CheckInQR.js
+++ app/features/checkin/CheckInQR.js
@@ -1,74 +1,117 @@
-import React, { useContext, useRef, useEffect } from 'react'
-import { View, Text, Alert, Dimensions } from 'react-native'
+import React, { useContext, useRef, useEffect } from "react";
+import { View, Text, Alert, Dimensions } from "react-native";
 import QRCodeScanner from "react-native-qrcode-scanner";
 import { request, PERMISSIONS } from "react-native-permissions";
 import { ThemeContext } from "../../contexts/ThemeContext";
-import { AuthContext } from '../../contexts/AuthContext';
-import { HackathonContext } from '../../state/hackathon';
-import { getApplication } from '../../api/registration';
+import { AuthContext } from "../../contexts/AuthContext";
+import { HackathonContext } from "../../state/hackathon";
+import { getApplication } from "../../api/registration";
+import { useIsFocused } from "@react-navigation/native";
 
 export function CheckInQR(props) {
-    const { dynamicStyles } = useContext(ThemeContext);
-    const { firebaseUser } = useContext(AuthContext);
-    const { state } = useContext(HackathonContext);
-    const hackathon = state.hackathon;
-    const scanner = useRef(null);
+  const { dynamicStyles } = useContext(ThemeContext);
+  const { firebaseUser } = useContext(AuthContext);
+  const { state } = useContext(HackathonContext);
+  const hackathon = state.hackathon;
+  const scanner = useRef(null);
+  const isFocused = useIsFocused();
 
-    useEffect(() => {
-        request(PERMISSIONS.IOS.CAMERA).then((result) => {
-            console.log(result);
-        });
-    }, []);
+  useEffect(() => {
+    const requestCameraPermission = async () => {
+      await request(PERMISSIONS.IOS.CAMERA);
+    };
 
-    const createAlert = (message) =>
-        Alert.alert("Error", message, [
-            {
-                text: "OK",
-                onPress: () => {
-                    if (scanner && scanner.current) {
-                        scanner.current.reactivate();
-                    }
-                },
-            },
-        ]);
+    requestCameraPermission();
+  }, []);
 
-    const onSuccess = async (e) => {
-        const json = JSON.parse(e.data);
-        if (json.uid) {
-            const token = await firebaseUser.getIdToken();
-            const res = await getApplication(token, hackathon.id, json.uid)
-            if (res.status !== 200) {
-                createAlert(res.message);
-            } else if (res.json.applications.length === 0) {
-                createAlert("This person never submitted an application to ", hackathon.name);
-            } else if (res.json.applications[0].status !== 'CONFIRMED') {
-                createAlert("This person's application is not confirmed yet. Current status: ", res.json.applications[0].status);
-            } else {
-                props.navigation.navigate("CheckInQR", {application: res.json})
-            }
-        } else {
-            createAlert("Invalid QR Code");
-        }
+  // Activate scanner whenever we navigate back to the screen
+  useEffect(() => {
+    if (scanner != null) {
+      scanner.current.reactivate();
+    }
+  }, [scanner, isFocused]);
+
+  const createAlert = (message) =>
+    Alert.alert("Error", message, [
+      {
+        text: "OK",
+        onPress: () => {
+          if (scanner && scanner.current) {
+            scanner.current.reactivate();
+          }
+        },
+      },
+    ]);
+
+  const onSuccess = async (e) => {
+    let json = {};
+
+    try {
+      json = JSON.parse(e.data);
+    } finally {
+      if (!json.uid) {
+        createAlert("Invalid QR Code");
+      }
     }
 
-    return (
-        <View style={[dynamicStyles.backgroundColor, {flex: 1}]}>
-            <View style={{ alignItems: 'center', paddingTop: 40 }}>
-                <Text style={{
-                    color: dynamicStyles.toggleText.color,
-                    fontFamily: "SpaceMono-Bold",
-                    fontSize: 20
-                }}>{'Scan Participant QR Code'}</Text>
-                <QRCodeScanner
-                    ref={scanner}
-                    reactivate={false}
-                    fadeIn={false}
-                    showMarker
-                    markerStyle={{ borderColor: "white", borderWidth: 2 }}
-                    onRead={onSuccess}
-                    cameraStyle={{ width: Dimensions.get("window").width - 30, alignSelf: 'center' }}
-                />
-            </View>
-        </View>
-    )
+    try {
+      const token = await firebaseUser.getIdToken();
+      const { data, status } = await getApplication(
+        token,
+        hackathon.id,
+        json.uid
+      );
+
+      if (status !== 200) {
+        createAlert(`Error contacting server: Status ${status}`);
+      } else if (data?.applications.length === 0) {
+        createAlert(
+          "This person never submitted an application to ",
+          hackathon.name
+        );
+      } else if (data?.applications[0].status !== "CONFIRMED") {
+        createAlert(
+          "This person's application is not confirmed yet. Current status: ",
+          data?.applications[0].status
+        );
+      } else if (!data?.applications[0].confirmationBranch) {
+        createAlert("This person does not have a valid confirmation branch");
+      } else {
+        props.navigation.navigate("CheckInNFC", {
+          application: data.applications[0],
+        });
+      }
+    } catch (e) {
+      createAlert(e.message);
+    }
+  };
+
+  return (
+    <View style={[dynamicStyles.backgroundColor, { flex: 1 }]}>
+      <View style={{ alignItems: "center", paddingTop: 40 }}>
+        <Text
+          style={{
+            color: dynamicStyles.toggleText.color,
+            fontFamily: "SpaceMono-Bold",
+            fontSize: 18,
+            marginBottom: 15,
+          }}
+        >
+          {"Scan Participant QR Code"}
+        </Text>
+        <QRCodeScanner
+          ref={scanner}
+          reactivate={false}
+          fadeIn={false}
+          showMarker
+          markerStyle={{ borderColor: "white", borderWidth: 2 }}
+          onRead={onSuccess}
+          cameraStyle={{
+            width: Dimensions.get("window").width - 30,
+            alignSelf: "center",
+          }}
+        />
+      </View>
+    </View>
+  );
 }
