567876d286237407a745b50a5846b61c10d5c3bd
Thaneesh Babu <gkthaneesh2@gmail.com>
2023-06-21 23:53:53 -0400

    Revert "android setup"
    This reverts commit 41903d3de85b0e955f5b7bc7a88f523cce3e174e.

--- android/app/src/main/java/com/appgt/MainApplication.java
+++ android/app/src/main/java/com/appgt/MainApplication.java
@@ -11,7 +11,6 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import io.invertase.firebase.config.ReactNativeFirebaseConfigPackage;
 
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 
@@ -30,7 +29,6 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          packages.add(new ReactNativeFirebaseConfigPackage());
           return packages;
         }
 
