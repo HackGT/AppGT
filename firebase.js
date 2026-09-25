9381dd0767e43d46700b8cf69a0aeac731356fc0
Jacob Ma <jacobdma218@gmail.com>
2026-09-25 14:52:39 -0400

    fix sign in persistence

--- firebase.js
+++ firebase.js
@@ -1,6 +1,12 @@
 import { initializeApp } from "firebase/app";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const app = initializeApp({
   apiKey: "AIzaSyB6-uF9OFtITWqFMqrvNiPVmBODAZ_IBXk",
   authDomain: "auth.hexlabs.org",
+});
+
+initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
 });
